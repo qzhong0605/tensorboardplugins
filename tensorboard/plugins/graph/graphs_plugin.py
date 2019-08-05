762fdd19ea1df2227da7d0f4bf0d22bba95e6c8a
yiiiaa <673411794@qq.com>
2019-08-05 16:43:45 +0800

    注册graphedut插件

--- tensorboard/plugins/graph/graphs_plugin.py
+++ tensorboard/plugins/graph/graphs_plugin.py
@@ -20,7 +20,6 @@ from __future__ import print_function
 
 import json
 import six
-import os
 from werkzeug import wrappers
 
 from tensorboard.backend import http_util
@@ -28,22 +27,14 @@ from tensorboard.backend import process_graph
 from tensorboard.backend.event_processing import plugin_event_accumulator as event_accumulator  # pylint: disable=line-too-long
 from tensorboard.compat.proto import config_pb2
 from tensorboard.compat.proto import graph_pb2
-from tensorboard.compat.proto import node_def_pb2
-from tensorboard.compat.proto import attr_value_pb2
-
 from tensorboard.plugins import base_plugin
-from tensorboard.plugins.graph_edit import graph_util
-from tensorboard.plugins.graph_edit import keras_util
-from tensorboard.plugins.graph_edit import c2graph_util
-from tensorboard.plugins.graph_edit import caffe_util
-from tensorboard.plugins.graph_edit import onnx_util
-from tensorboard.plugins.graph_edit import onnx_write_util
-
+from tensorboard.plugins.graph import graph_util
+from tensorboard.plugins.graph import keras_util
 from tensorboard.util import tb_logging
 
 logger = tb_logging.get_logger()
-# logger.setLevel(tb_logging.logging.INFO)
-_PLUGIN_PREFIX_ROUTE = 'graphedit'
+
+_PLUGIN_PREFIX_ROUTE = 'graphs'
 
 # The Summary API is implemented in TensorFlow because it uses TensorFlow internal APIs.
 # As a result, this SummaryMetadata is a bit unconventional and uses non-public
@@ -56,17 +47,9 @@ _PLUGIN_NAME_RUN_METADATA_WITH_GRAPH = 'graph_run_metadata_graph'
 _PLUGIN_NAME_KERAS_MODEL = 'graph_keras_model'
 
 
-class GraphEditPlugin(base_plugin.TBPlugin):
-  """GraphEdit plugins for TensorBoard.
+class GraphsPlugin(base_plugin.TBPlugin):
+  """Graphs Plugin for TensorBoard."""
 
-  For visualization, the Graph IR(Intermediate Representation) is on graph_pb2.
-  For all other model graph, it must be converted into tensorboard IR.
-
-  The workflow is to load an graph representation(including empty graph if not
-  including graph definition) and edit on the dashboard of tensorboard
-
-  Currently, the supported model type including caffe2/caffe, pytorch, onnx, tensorflow.
-  """
   plugin_name = _PLUGIN_PREFIX_ROUTE
 
   def __init__(self, context):
@@ -76,68 +59,21 @@ class GraphEditPlugin(base_plugin.TBPlugin):
       context: A base_plugin.TBContext instance.
     """
     self._multiplexer = context.multiplexer
-    self._tb_graph = None
 
   def get_plugin_apps(self):
     return {
-        '/init':self.init_graph,
-        '/save': self.save_model,
-        '/edit': self.edit_model,
         '/graph': self.graph_route,
         '/info': self.info_route,
         '/run_metadata': self.run_metadata_route,
-        '/load': self.parse_from_model,
-        '/op_intro':self.op_intro,
-        '/op_default':self.op_default,
     }
 
-  @wrappers.Request.application
-  def parse_from_model(self, request):
-      """ It used to parse model file and convert it to tensorboard IR
-      """
-      model_type = request.args.get("source_type")
-      file_type = request.args.get("file_type")
-      model_file = request.args.get('model_file')
-      if model_type == "torch":
-          pass
-      elif model_type == "caffe2":
-          predict_net = request.args.get("predict_net")
-          init_net = request.args.get("init_net")
-          if not (os.path.exists(predict_net) and os.path.exists(init_net)):
-              # send a response to frontend and report that model file doesnot exist
-              pass
-          self._tb_graph = c2graph_util.C2Graph(predict_net, file_type, init_net)
-      elif model_type == "caffe":
-          self._tb_graph = caffe_util.CaffeGraph(model_file, file_type)
-      elif model_type == "onnx":
-          self._tb_graph = onnx_util.OnnxGraph(model_file, file_type)
-      elif model_type == "tf":
-          pass
-      else:
-          # send a response to frontend and report model type error
-          pass
-
-      if not os.path.exists(model_file):
-          # send a response to frontend and report file not existing
-          pass
-
-      self._tb_graph.ConvertNet()
-      graph = self._tb_graph._tb_graph
-      self.graph = graph
-      return http_util.Respond(request,str(graph) ,'text/x-protobuf')
-
-  @wrappers.Request.application
-  def init_graph(self, request):
-    self.graph = graph_pb2.GraphDef()
-    return http_util.Respond(request, 'init', 'text/plain')
-
   def is_active(self):
-      """ GraphEdit plugin is always active """
-      return True
+    """The graphs plugin is active iff any run has a graph."""
+    return bool(self._multiplexer and self.info_impl())
 
   def frontend_metadata(self):
-    return super(GraphEditPlugin, self).frontend_metadata()._replace(
-        element_name='tf-graph-edit-dashboard',
+    return super(GraphsPlugin, self).frontend_metadata()._replace(
+        element_name='tf-graph-dashboard',
         # TODO(@chihuahua): Reconcile this setting with Health Pills.
         disable_reload=True,
     )
@@ -233,7 +169,6 @@ class GraphEditPlugin(base_plugin.TBPlugin):
         graph_util.combine_graph_defs(graph, func_graph.pre_optimization_graph)
     else:
       graph = self._multiplexer.Graph(run)
-      self.graph = graph
 
     # This next line might raise a ValueError if the limit parameters
     # are invalid (size is negative, size present but key absent, etc.).
@@ -316,87 +251,3 @@ class GraphEditPlugin(base_plugin.TBPlugin):
     else:
       return http_util.Respond(request, '404 Not Found', 'text/plain',
                                code=404)
-
-  @wrappers.Request.application
-  def save_model(self, request):
-    model_type = request.args.get('type')
-    graph = self.graph
-    logger.warn(len(graph.node))
-    if model_type=='onnx':
-      onnx_write_util.SaveInOnnxModel(graph)
-    return http_util.Respond(request, 'save progress success', 'text/plain')
-
-  @wrappers.Request.application
-  def edit_model(self, request):
-    eidt_type = request.args.get('type')
-    # logger.warn(eidt_type)
-    data = json.loads(request.args.get('data'))
-    message = ''
-
-    graph = self.graph
-
-    if eidt_type == 'add_node':
-      # logger.warn(data['attr'])
-      newNode = node_def_pb2.NodeDef()
-      newNode.name = data['name']
-      newNode.op = data['op']
-      # 根据返回的属性类型，设置相应值
-      # for attr_item in data['attr']:
-      #   logger.warn(attr_item)
-      #   newAttr = attr_value_pb2.AttrValue()
-      #   newNode.attr.get_or_create()
-      # logger.warn(newNode)
-      graph.node.extend([newNode])
-      message = 'ok'
-
-    if eidt_type == 'edit':
-      # logger.warn(data['attr'])
-      for i in range(0, len(graph.node)):
-        if graph.node[i].name == data['name']:
-          tmp_node_attrs = graph.node[i].attr
-          for attr_item in data['attr']:
-            attr_to_edit = tmp_node_attrs.get_or_create(attr_item['key'])
-            # logger.warn(attr_to_edit)
-            # 根据返回的属性类型，设置相应值
-          break
-      message = 'ok'
-
-    if eidt_type == 'delete_node':
-      delete_pos = -1
-      for i in range(0, len(graph.node)):
-        tmp_node = graph.node[i]
-        if data['op'] in tmp_node.input:
-          tmp_node.input.remove(data['op'])
-        if tmp_node.name == data['op']:
-          delete_pos = i
-      del graph.node[delete_pos]
-      message = 'ok'
-
-    if eidt_type == 'add_edge':
-      for i in range(0, len(graph.node)):
-        if graph.node[i].name == data['w']:
-          graph.node[i].input.append(data['v'])
-      message = 'ok'
-
-    if eidt_type == 'delete_edge':
-      for i in range(0, len(graph.node)):
-        if graph.node[i].name == data['w']:
-          graph.node[i].input.remove(data['v'])
-      message = 'ok'
-
-    return http_util.Respond(request, message, 'text/plain')
-
-  @wrappers.Request.application
-  def op_intro(self, request):
-    op = request.args.get('op')
-    logger.warn(op)
-    return http_util.Respond(request, op, 'text/plain')
-
-  @wrappers.Request.application
-  def op_default(self, request):
-    output = request.args.get('output')
-    logger.warn(output)
-    # default 属性默认值信息
-    default = [{'name':'kernel','type':'a','default':'0'},{'name':'stride','type':'b','default':'0'}]
-    data = json.dumps(default)
-    return http_util.Respond(request, data, 'application/json')
